3ef46d289f1cf1c93fdd9902fdff42a67dfe2125
MamunAmin <mamun.amin2020@gmail.com>
2021-07-05 12:52:31 +0600

    install tailwind JIT

--- components/Heading/Heading.jsx
+++ components/Heading/Heading.jsx
@@ -3,7 +3,7 @@ const Heading = () => {
     return (
         <div className="flex flex-row items-end justify-center mt-14 ">
             <img src="/word.png" className="h-16 w-14"/>
-            <h1 className="text-5xl font-radley px-1"><span className="sss">WORD</span> To <span>PDF</span></h1>
+            <h1 className="text-5xl font-radley px-1"><span className="text-[#295497]">WORD</span> To <span className="text-[#DD3537]" >PDF</span></h1>
             <img src="/pdf.png" className="h-16 w-14"/>
         </div>
     );
